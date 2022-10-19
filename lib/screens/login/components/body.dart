7e48a7a6f5308b88dde21b095ec56bc0c0d2892a
Ridwan Hidayat <ridwan.nurulhidayat@gmail.com>
2022-10-19 07:12:08 +0700

    verse 2 login

--- lib/screens/login/components/body.dart
+++ lib/screens/login/components/body.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_login/components/text_field_container.dart';
+import 'package:flutter_login/components/rounded_input_field.dart';
 import 'package:flutter_login/constants.dart';
 import 'package:flutter_login/screens/login/components/background.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_login/components/text_field_container.dart';
 
 class Body extends StatelessWidget {
   const Body({Key? key}) : super(key: key);
@@ -28,13 +29,39 @@ class Body extends StatelessWidget {
             icon: Icons.person,
             onChanged: (value) {},
           ),
-          RoundedInputField(
-            hintText: 'Password',
-            icon: Icons.lock,
+          RoundedPasswordField(
             onChanged: (value) {},
           )
         ],
       ),
     );
+  }
+}
+
+class RoundedPasswordField extends StatelessWidget {
+  const RoundedPasswordField({
+    Key? key,
+    required this.onChanged,
+  }) : super(key: key);
+  final ValueChanged<String> onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFieldContainer(
+        child: TextField(
+      obscureText: true,
+      onChanged: onChanged,
+      decoration: const InputDecoration(
+          hintText: 'Password',
+          icon: Icon(
+            Icons.lock,
+            color: kPrimaryColor,
+          ),
+          suffixIcon: Icon(
+            Icons.visibility,
+            color: kPrimaryColor,
+          ),
+          border: InputBorder.none),
+    ));
   }
 }
